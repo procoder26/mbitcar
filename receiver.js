c1422ae787a680727da006590d5933b2df338468
procoder26 <116507975+procoder26@users.noreply.github.com>
2024-09-16 08:08:18 -0400

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -4,13 +4,14 @@
 radio.setGroup(181)
 servos.P0.setAngle(90)
 servos.P1.setAngle(90)
+let speed = 20
 
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        servos.P1.setAngle(110)
+        servos.P1.setAngle(90+speed)
     }
     if (receivedNumber == 2) {
-        servos.P1.setAngle(70)
+        servos.P1.setAngle(90-speed)
     }
     if (receivedNumber == 3) {
         servos.P1.setAngle(90)
