e0cc6c0f9545a943438a75eb871b45d224c9d9d2
procoder26 <116507975+procoder26@users.noreply.github.com>
2024-09-16 17:00:03 -0400

    allows button to be held to move

--- transmitter.js
+++ transmitter.js
@@ -1,23 +1,22 @@
-//DOWNLOAD SERVO EXTENSION BEFORE RUN ON BOTH MICROBITS
-//HAVE FUN!
-radio.setGroup(181)
+//transmitter
 
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(3)
-})
+radio.setGroup(181)
 
 basic.forever(function () {
     radio.sendValue("rotate", pins.map(
-    input.acceleration(Dimension.X),
-    -1023,
-    1023,
-    30,
-    150
+        input.acceleration(Dimension.X),
+        -1023,
+        1023,
+        50,
+        130
     ))
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    }
+    else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    }
+    else {
+        radio.sendNumber(3)
+    }
 })
