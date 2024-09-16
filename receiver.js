453af228d025610b94d0c1e6d0995113f2ca3f1f
procoder26 <116507975+procoder26@users.noreply.github.com>
2024-09-16 17:01:53 -0400

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -7,12 +7,21 @@ let speed = 20
 
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
+        servos.P1.setAngle(90)
         servos.P1.setAngle(90 + speed)
+        
     }
     if (receivedNumber == 2) {
+        servos.P1.setAngle(90)
         servos.P1.setAngle(90 - speed)
     }
     if (receivedNumber == 3) {
         servos.P1.setAngle(90)
+    }
+})
+
+radio.onReceivedValue(function (name, value) {
+    if (name == "rotate") {
+        servos.P0.setAngle(value)
     }
 })
